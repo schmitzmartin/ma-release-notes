7718b9f941cab90dc5d5d71717d75e579086cb6d
Martin Schmitz <martin.schmitz@hey.com>
2020-06-29 17:28:06 +0200

    First draft for Release Notes 5_6 (#4)
    * First draft for Release Notes 5_6
    
    - Adds Excel-Export
    - Adds field types "Link" and "Email"
    - Adds improved table behaviour
    
    * Fixes about-releases to 8-week cycle
    
    * Adds more Release Notes
    
    * Adds Flowchart as feature
    
    * Edits Excel Export-text
    
    * Adds more release notes
    
    * adds excel export (tagged content)
    
    * Adds missing translations
    
    * Adds roles (and image) to rn_56
    
    * Fixes error?
    
    * Removes tag line again
    
    * last try
    
    * Adds rn56 as draft
    
    Co-authored-by: Martin Schmitz <martin.v.schmitz@gmail.com>

--- src/filters/date-filter.js
+++ src/filters/date-filter.js
@@ -1,15 +1,9 @@
 // Stolen from https://stackoverflow.com/a/31615643
-const appendSuffix = n => {
-  var s = ['th', 'st', 'nd', 'rd'],
-    v = n % 100;
-  return n + (s[(v - 20) % 10] || s[v] || s[0]);
-};
 
 module.exports = function dateFilter(value) {
   const dateObject = new Date(value);
 
   const months = ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'];
-  const dayWithSuffix = appendSuffix(dateObject.getDate());
 
   return `${dateObject.getDate()}. ${months[dateObject.getMonth()]} ${dateObject.getFullYear()}`;
 };
