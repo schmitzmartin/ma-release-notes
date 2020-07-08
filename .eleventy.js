6dfca8b1f0417f0867205279d0175c628b8bb0a1
Martin Schmitz <martin.schmitz@hey.com>
2020-07-08 10:04:05 +0200

    Adds Youtube Embed (#14)
    Also adds space between elements contained in a list element

--- .eleventy.js
+++ .eleventy.js
@@ -1,53 +1,54 @@
-const rssPlugin = require('@11ty/eleventy-plugin-rss');
-const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
-const fs = require('fs');
+const rssPlugin = require("@11ty/eleventy-plugin-rss");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const fs = require("fs");
+const embedYouTube = require("eleventy-plugin-youtube-embed");
 
 // Import filters
-const dateFilter = require('./src/filters/date-filter.js');
-const markdownFilter = require('./src/filters/markdown-filter.js');
-const w3DateFilter = require('./src/filters/w3-date-filter.js');
+const dateFilter = require("./src/filters/date-filter.js");
+const markdownFilter = require("./src/filters/markdown-filter.js");
+const w3DateFilter = require("./src/filters/w3-date-filter.js");
 
 // Import transforms
-const htmlMinTransform = require('./src/transforms/html-min-transform.js');
-const parseTransform = require('./src/transforms/parse-transform.js');
+const htmlMinTransform = require("./src/transforms/html-min-transform.js");
+const parseTransform = require("./src/transforms/parse-transform.js");
 
 // Import data files
-const site = require('./src/_data/site.json');
+const site = require("./src/_data/site.json");
 
 module.exports = function(config) {
   // Filters
-  config.addFilter('dateFilter', dateFilter);
-  config.addFilter('markdownFilter', markdownFilter);
-  config.addFilter('w3DateFilter', w3DateFilter);
+  config.addFilter("dateFilter", dateFilter);
+  config.addFilter("markdownFilter", markdownFilter);
+  config.addFilter("w3DateFilter", w3DateFilter);
 
   // Layout aliases
-  config.addLayoutAlias('home', 'layouts/home.njk');
+  config.addLayoutAlias("home", "layouts/home.njk");
 
   // Transforms
-  config.addTransform('htmlmin', htmlMinTransform);
-  config.addTransform('parse', parseTransform);
+  config.addTransform("htmlmin", htmlMinTransform);
+  config.addTransform("parse", parseTransform);
 
   // Passthrough copy
-  config.addPassthroughCopy('src/fonts');
-  config.addPassthroughCopy('src/images');
+  config.addPassthroughCopy("src/fonts");
+  config.addPassthroughCopy("src/images");
   // config.addPassthroughCopy('src/js');
-  config.addPassthroughCopy('src/admin/config.yml');
-  config.addPassthroughCopy('src/admin/previews.js');
-  config.addPassthroughCopy('node_modules/nunjucks/browser/nunjucks-slim.js');
-  config.addPassthroughCopy('src/robots.txt');
+  config.addPassthroughCopy("src/admin/config.yml");
+  config.addPassthroughCopy("src/admin/previews.js");
+  config.addPassthroughCopy("node_modules/nunjucks/browser/nunjucks-slim.js");
+  config.addPassthroughCopy("src/robots.txt");
 
   const now = new Date();
 
   // Custom collections
   const livePosts = post => post.date <= now && !post.data.draft;
-  config.addCollection('posts', collection => {
+  config.addCollection("posts", collection => {
     return [
-      ...collection.getFilteredByGlob('./src/posts/*.md').filter(livePosts)
+      ...collection.getFilteredByGlob("./src/posts/*.md").filter(livePosts)
     ].reverse();
   });
 
-  config.addCollection('postFeed', collection => {
-    return [...collection.getFilteredByGlob('./src/posts/*.md').filter(livePosts)]
+  config.addCollection("postFeed", collection => {
+    return [...collection.getFilteredByGlob("./src/posts/*.md").filter(livePosts)]
       .reverse()
       .slice(0, site.maxPostsPerPage);
   });
@@ -55,14 +56,15 @@ module.exports = function(config) {
   // Plugins
   config.addPlugin(rssPlugin);
   config.addPlugin(syntaxHighlight);
+  config.addPlugin(embedYouTube);
 
   // 404
   config.setBrowserSyncConfig({
     callbacks: {
       ready: function(err, browserSync) {
-        const content_404 = fs.readFileSync('dist/404.html');
+        const content_404 = fs.readFileSync("dist/404.html");
 
-        browserSync.addMiddleware('*', (req, res) => {
+        browserSync.addMiddleware("*", (req, res) => {
           // Provides the 404 content without redirect.
           res.write(content_404);
           res.end();
@@ -73,8 +75,8 @@ module.exports = function(config) {
 
   return {
     dir: {
-      input: 'src',
-      output: 'dist'
+      input: "src",
+      output: "dist"
     },
     passthroughFileCopy: true
   };
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Release Notes for Q.wiki
+
+## Bilder einbetten
+
+```md
+![Alt-Text](/pfad/zum/bild.jpg "Ich stehe dann unterm Bild drunter")
+```
+
+## YouTube-Videos embedden
+
+einfach die Video-URL (z.B. https://www.youtube.com/watch?v=1svA2sGhDEE) als einzigen Inhalt in eine Zeile kopieren
